e85fd51b8a6d71c385a10fc2c57cb3e662f0d4ff
sciwhiz12 <arnoldnunag12@gmail.com>
2021-11-13 20:09:05 +0800

    Move list end tag to next line

--- src/main/java/net/minecraftforge/event/entity/EntityMobGriefingEvent.java
+++ src/main/java/net/minecraftforge/event/entity/EntityMobGriefingEvent.java
@@ -31,7 +31,8 @@ import net.minecraftforge.eventbus.api.Event.HasResult;
  * <ul>
  * <li>{@link Result#ALLOW} means this instance of mob griefing is allowed.</li>
  * <li>{@link Result#DEFAULT} means the {@code mobGriefing} game rule is used to determine the behaviour.</li>
- * <li>{@link Result#DENY} means this instance of mob griefing is not allowed.</li></ul>
+ * <li>{@link Result#DENY} means this instance of mob griefing is not allowed.</li>
+ * </ul>
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
  */
 @HasResult
